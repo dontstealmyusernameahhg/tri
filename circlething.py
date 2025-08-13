65bf45860cc584a88492fb7234d25557646e941a
dontstealmyusernameahhg <qwertyuiopreeyansh@gmail.com>
2025-08-13 18:27:42 +1200

    hello circle

---
+++ circlething.py
@@ -0,0 +1,19 @@
+# circlething.py
+# This script defines a Circle class with methods to calculate area and perimeter.
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def area(self):
+        return math.pi * self.radius * self.radius
+        
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+        
+r = float(input("Enter radius: "))
+c = Circle(r)
+
+print("Area:", c.area())
+print("Perimeter:", c.perimeter())
